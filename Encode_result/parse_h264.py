7fa2b6554a7fd6a512cccd3b883109dd16d8d443
wangsen <17714924649@163.com>
2020-07-10 14:55:31 +0800

    add h264 file

---
+++ Encode_result/parse_h264.py
@@ -0,0 +1,19 @@
+import av
+import sys
+
+
+def h264_to_jpg():
+    input_file_name = "encode_output.h264"
+    container = av.open(input_file_name)
+    print("container:", container)
+    print("container.streams:", container.streams)
+    print("container.format:", container.format)
+
+    for frame in container.decode(video=0):
+        print("process frame: %04d (width: %d, height: %d)" %
+              (frame.index, frame.width, frame.height))
+        frame.to_image().save("frame-%04d.jpg" % frame.index)
+
+
+if __name__ == "__main__":
+    h264_to_jpg()
